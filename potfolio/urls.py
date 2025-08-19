153af4c716cf123435a1dfa69bd3a273db79a1dc
Ajjitech-Design <asorirejibola52@gmail.com>
2025-08-19 15:32:58 +0100

    initial commit

--- potfolio/urls.py
+++ potfolio/urls.py
@@ -27,5 +27,5 @@ urlpatterns = [
     path('recent/', include('JOBS.urls')),
 ]
 
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT )
+if settings.DEBUG:
+     urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT )
